d93bbc8560f05c124c3a3a49e4adc29818a5a7f5
Eduard <eduardstancu@yahoo.com>
2020-08-08 11:01:57 +0300

    changes001

--- src/App.js
+++ src/App.js
@@ -3,12 +3,45 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    string: 'Hello Man',
+    name: 'gigel',
+    counter: 0,
+    amici: ['Ionescu', 'Popescu', 'Andreescu', 'Muiescu'],
+    txt: '',
+  };
+
+  apasaButton = (e) => {
+    //alert(this.state.string);
+    this.setState({ name: 'vasile' });
+    this.setState({ string: 'asta-i stringul functiei apasaButton' });
+    this.setState({ counter: this.state.counter + 1 });
+    const newTxt = this.arataText('bla bla bla text');
+    this.setState({ txt: newTxt });
+  };
+  arataText(gigel) {
+    return gigel + ' continuarea textului';
+  }
+
   render() {
     return (
       <div className='App'>
         <header className='App-header'>This is the Header</header>
         <div>
-          <p>This is my shitty React app</p>
+          <p>{this.state.string}</p>
+          <p style={{ color: 'blue', fontFamily: 'arial', fontWeight: '100' }}>
+            My Name is {this.state.name} counter {this.state.counter}
+          </p>
+          <button onClick={this.apasaButton}>Apasa bai tarane!</button>
+          <button
+            onClick={() => {
+              this.setState({ string: 'am schimbat stringul' });
+              this.setState({ txt: '' });
+            }}
+          >
+            Schimba
+          </button>
+          <p>{this.state.txt}</p>
         </div>
       </div>
     );
