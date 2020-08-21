ceb0fe124f00cfbebe4df29495cc0be53479db9b
Eduard <eduardstancu@yahoo.com>
2020-08-22 01:15:10 +0300

    Updates

---
+++ precache-manifest.4ebeaf5590d1c2f1c67ae023de75caf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9881af0bd387aea7fd28d868c59d7324",
+    "url": "/prj36-lr-mnst-rldx/index.html"
+  },
+  {
+    "revision": "8ef755a7daa6640a1e74",
+    "url": "/prj36-lr-mnst-rldx/static/css/main.d657224a.chunk.css"
+  },
+  {
+    "revision": "3912722782fb458c8a39",
+    "url": "/prj36-lr-mnst-rldx/static/js/2.e9414357.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prj36-lr-mnst-rldx/static/js/2.e9414357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef755a7daa6640a1e74",
+    "url": "/prj36-lr-mnst-rldx/static/js/main.5cd1425e.chunk.js"
+  },
+  {
+    "revision": "70be5efc08e1cd89edc2",
+    "url": "/prj36-lr-mnst-rldx/static/js/runtime-main.b2d2f4c3.js"
+  }
+]);
